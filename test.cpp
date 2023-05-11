7efeb2e1f98dc0ffb12a660afeb4b13b2d4d36db
FOOZBY <alexeysharonin15@gmail.com>
2023-05-11 08:34:04 +0300

    Добавьте файлы проекта.

---
+++ test.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    srand(time(nullptr)); // use current time as seed for random generator
+    cout << "enter num of vershin: ";
+    int N, a;
+    cin >> N;
+
+    // roll 6-sided dice 20 times
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            a = rand() % 10;
+            if (a < 5)
+            {
+                a = 0;
+            }
+            else
+            {
+                a = (rand() % 9)+1;
+            }
+            if (i == j)
+                a = 0;
+            cout << a << " ";
+        }
+        cout << endl;
+    }
+    system("pause");
+}
